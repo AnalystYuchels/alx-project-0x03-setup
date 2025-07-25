fdcf315f50752a46a877925123ef5af458ba37f8
AnalystYuchels <uche4xploit@gmail.com>
2025-07-25 05:14:55 +0100

    Updated index.tsx file

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -15,8 +15,9 @@ export default function Home() {
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
       {/* Welcome Message */}
       <h1>
-        Welcome to Splash App!
+        Welcome to Splash App
       </h1>
+      
       <p className="text-lg text-gray-600 mb-8">
         Your one-stop platform for everything AI you need. Start exploring by
         navigating to our features below.
